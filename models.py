84d3b60c2ccd076368e8710e24cc3a8056f04951
Mamak-Amak <105986370+Mamak-Amak@users.noreply.github.com>
2023-03-15 20:47:49 +0500

    lesson_6 (#6)

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Article(models.Model):
@@ -7,5 +8,22 @@ class Article(models.Model):
     text = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     create = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+
+    name = models.CharField(max_length=64, unique=True)
+    birthday_year = models.PositiveIntegerField()
+    def __str__(self):
+        return self.name
+    
+
+class Book(models.Model):
+
+    name = models.CharField(max_length=64, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
