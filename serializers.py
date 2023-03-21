41eb12466011b54ce82c5e8d9c3348b84c47cee2
Mamak-Amak <105986370+Mamak-Amak@users.noreply.github.com>
2023-03-21 19:19:05 +0500

    lesson_8

--- serializers.py
+++ serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
 from .models import Author, Book
 
-
 class AuthorSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Author
         fields = '__all__'
 
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fieds = '__all__'
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+class BookSerializerBase(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer):
     author = AuthorSerializer()
