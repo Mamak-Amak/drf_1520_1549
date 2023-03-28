8f73812dae5ac37a5ee3a2219254ba94f8f9acb5
Mamak-Amak <105986370+Mamak-Amak@users.noreply.github.com>
2023-03-28 20:29:37 +0500

    Урок 10. GraphQl

--- urls.py
+++ urls.py
@@ -1,8 +1,47 @@
+from graphene_django.views import GraphQLView
 from django.urls import path
-from .views import UserListAPIView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken import views
+from mainapp.views import AuthorViewSet, BookViewSet
 
-
-app_name = 'userapp'
+router = routers.DefaultRouter()
+router.register('authors', AuthorViewSet)
+router.register('books', BookViewSet)
 urlpatterns = [
-    path('', UserListAPIView.as_view()),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library",
+        default_version='0.1',
+        description="Documentation to out project",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
+    path("graphql/", GraphQLView.as_view(graphiql=True)),
+]
+
+
+    
+    
+
